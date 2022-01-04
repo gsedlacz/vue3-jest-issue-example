c6a27e7cc3b0116773afbc82e2988fac2ec96c7d
gsedlaczek <gerrit.sedlaczek@cosinex.de>
2022-01-04 10:41:30 +0100

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TestWorld from "@/components/TestWorld.vue";
+
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+	<TestWorld/>
+</template>
